9fd77331d4c082a641f38c9d38294f05b02c4bc4
amitfagna2007-rgb <amitfagna2007@gmail.com>
2026-09-19 12:57:23 +0530

    Refresh PWA cache after print layout fix

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='sarasvati-paper-maker-v12';
+const CACHE='sarasvati-paper-maker-v13';
 const ASSETS=['./','./index.html','./manifest.webmanifest','./icon.svg','./school_logo.svg','./sw.js'];
 
 self.addEventListener('install',e=>e.waitUntil(
