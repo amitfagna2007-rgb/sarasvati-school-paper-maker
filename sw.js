4d2770c03086080278adc15d3b472e1cb367cf90
amitfagna2007-rgb <amitfagna2007@gmail.com>
2026-09-21 14:10:52 +0530

    Refresh PWA cache after voice fix

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='sarasvati-paper-maker-v17';
+const CACHE='sarasvati-paper-maker-v18';
 const ASSETS=['./','./index.html','./manifest.webmanifest','./icon.svg','./school_logo.svg','./sw.js'];
 
 self.addEventListener('install',e=>e.waitUntil(
