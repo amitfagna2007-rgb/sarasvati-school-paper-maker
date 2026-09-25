ee8ebd2cc2c70f0401ce982c163f16c190a5dcae
amitfagna2007-rgb <amitfagna2007@gmail.com>
2026-09-25 18:55:58 +0530

    Refresh cache for restored stable app with voice update

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='sarasvati-paper-maker-v37';
+const CACHE='sarasvati-paper-maker-v38';
 const ASSETS=['./','./index.html','./manifest.webmanifest','./icon.svg','./school_logo.svg','./sw.js','./version.json'];
 
 self.addEventListener('message',e=>{if(e.data&&e.data.type==='SKIP_WAITING')self.skipWaiting()});
